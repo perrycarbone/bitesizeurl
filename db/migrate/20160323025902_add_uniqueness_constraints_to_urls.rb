57cf4bd30e761bb37716483cc5cda23e390e91fe
Perry Carbone <perrycarb@gmail.com>
2016-03-22 23:02:20 -0400

    add uniqueness constraints to urls

---
+++ db/migrate/20160323025902_add_uniqueness_constraints_to_urls.rb
@@ -0,0 +1,6 @@
+class AddUniquenessConstraintsToUrls < ActiveRecord::Migration
+  def change
+    add_index :urls, :full_url, unique: true
+    add_index :urls, :short_url, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322170547) do
+ActiveRecord::Schema.define(version: 20160323025902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,5 +23,8 @@ ActiveRecord::Schema.define(version: 20160322170547) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "urls", ["full_url"], name: "index_urls_on_full_url", unique: true, using: :btree
+  add_index "urls", ["short_url"], name: "index_urls_on_short_url", unique: true, using: :btree
 
 end
